a5e449e8ef5e0456fe7f19e4548e2135021b35e2
Sagar Munjal <munjal.sagar@gmail.com>
2016-06-14 20:07:00 +0530

    calculator simplified

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-function sum(a,b){
-alert(a+b);
+function run1(){
+document.calc.result.value += "1";
 }
-
-function diff(a,b){
-return a-b;
+function run2(){
+document.calc.result.value += "2";
 }
-
-function product(a,b){
-return a*b;
+function run3(){
+document.calc.result.value += "3";
 }
-
-function divide(a,b){
-return a/b;
+function plus(){
+document.calc.result.value += "+";
 }
-
-function getVal()
-{
-   alert("sss");
+function run4(){
+document.calc.result.value += "4";
+}
+function run5(){
+document.calc.result.value += "5";
+}
+function run6(){
+document.calc.result.value += "6";
+}
+function diff(){
+document.calc.result.value += "-";
+}
+function run7(){
+document.calc.result.value += "7";
+}
+function run8(){
+document.calc.result.value += "8";
+}
+function run9(){
+document.calc.result.value += "9";
+}
+function divide(){
+document.calc.result.value += "/";
+}
+function remainder(){
+document.calc.result.value += "%";
+}
+function clear(){
+  document.calc.result.value = null;
+}
+function equalsto(){
+var evalo = eval(document.calc.result.value)
+document.calc.result.value = evalo
 }
