e1c3e95f8afb1105dae3b88b2763762cdb51d030
Sagar Munjal <munjal.sagar@gmail.com>
2016-06-14 14:27:35 +0530

    changes made to script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function sum(a,b){
+alert(a+b);
+}
+
+function diff(a,b){
+return a-b;
+}
+
+function product(a,b){
+return a*b;
+}
+
+function divide(a,b){
+return a/b;
+}
+
+function getVal()
+{
+   alert("sss");
+}
